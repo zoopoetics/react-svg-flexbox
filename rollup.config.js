69dd8bbd4165983e3bc37f8c0530ba863e5d8ac5
peter <zoopoetics@gmail.com>
2019-02-03 17:26:21 -0800

    Refresh Rollup configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
+import {uglify} from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/index.js',
@@ -8,23 +8,12 @@ export default {
     file: 'dist/bundle.js',
     format: 'cjs',
   },
-  external: [
-    'css-layout',
-    'prop-types',
-    'react',
-    'react-dom',
-  ],
+  external: ['css-layout', 'prop-types', 'react', 'react-dom'],
   plugins: [
     resolve(),
     babel({
-      plugins: ['external-helpers'],
-      presets: [
-        ['env', {
-          'modules': false,
-        }],
-        'stage-0',
-        'react'
-      ]
+      runtimeHelpers: true,
+      presets: ['react-app'],
     }),
     uglify(),
   ],
